2dcc65197dd47b8e325927fb22aedbd11841ddba
Ezeikpe <juscraft@yahoo.com>
2022-09-09 14:54:10 -0700

    Commit again

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,22 +5,18 @@
  * Return: 0 (Success)
  */
 int main(void)
-
 {
-int i;
+	int i;
 
-for (i = 48; i < 58; i++)
-{
-putchar(i);
-}
-for (i = 'a'; i <= 'f'; i++)
-{
-						
-putchar(i);
-}
-
-putchar('\n');
-
-return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}	
+	for (i = 'a'; i <= 'f'; i++)
+	{					
+		putchar(i);
+	}
+		putchar('\n');
+	return (0);
 
 }
