a7b4acba6fd020080216d0ce5aa3b206438b694a
Ezeikpe <juscraft@yahoo.com>
2022-09-09 19:59:02 -0700

    Commit again

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,20 @@
 /*
  * main - Print all numbers of base 16 in lowercase
  *
- * Return: 0 (Success)
+ * Return: 0 (true)
  */
 int main(void)
 {
-       	int i;
+int i;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}	
-	for (i = 'a'; i <= 'f'; i++)
-	{					
-		putchar(i);
-	}
- 	putchar('\n');
-	return (0);
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}	
+for (i = 'a'; i <= 'f'; i++)
+{					
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
