e75b6a5fc9d6a210d527d83267220b9bc9a2d5f8
Ezeikpe <juscraft@yahoo.com>
2022-09-09 18:40:39 -0700

    Commit again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,34 +3,34 @@
 /**
  * main - All possible combinations of 2 digits
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (True)
  */
 int main(void)
-
 {
-int a, b;
+	int a, b;
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a < b)
+			{
 
-for (a = '0'; a <= '9'; a++)
-{
-for (b = '0'; b <= '9'; b++)
-{
-if (a < b)									{
+				putchar(a);
+				putchar(b);
 
-putchar(a);
-putchar(b);
+				if (a != '8' || (a == '8' && b != '9'))
+				{
 
-if (a != '8' || (a == '8' && b != '9')
-}
+					putchar(',');
+					putchar(' ');
 
-putchar(',');
-putchar(' ');
-										}
-}
-}
-}
+				}
+			}
 
-putchar('\n');
+		}	
 
-return (0);
+	}	
+	putchar('\n');
+	return (0);
 
 }
