eae0ae72c3a10e42ac6e5414119162f6a436312e
Ezeikpe <juscraft@yahoo.com>
2022-09-09 17:21:37 -0700

    Commit again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - All possible combinations of 2 digits
  *
@@ -9,28 +8,29 @@
 int main(void)
 
 {
-	int a, b;
+int a, b;
 
-	for (a = '0'; a <= '9'; a++)
-	{
-	for (b = '0'; b <= '9'; b++)
-	{
-	if (a < b)																{	
-	putchar(a);
-  	putchar(b);
-        if (a != '8' || (a == '8' && b != '9')
-	}
-	putchar(',');
-	putchar(' ');
-																																}
-	}
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+if (a < b)									{
 
-	}
+putchar(a);
+putchar(b);
 
-	}
+if (a != '8' || (a == '8' && b != '9')
+}
 
-	putchar('\n');
+putchar(',');
+putchar(' ');
+										}
+}
+}
+}
 
-	return (0);
+putchar('\n');
+
+return (0);
 
 }
