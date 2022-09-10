09f18704840084018f23d33de3286463abe8e6da
Ezeikpe <juscraft@yahoo.com>
2022-09-09 18:52:28 -0700

    Commit again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int a, b;
+
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
@@ -29,8 +30,9 @@ int main(void)
 
 		}	
 
-	}	
-	putchar('\n');
-	return (0);
+	}
+
+putchar('\n');
+return (0);
 
 }
