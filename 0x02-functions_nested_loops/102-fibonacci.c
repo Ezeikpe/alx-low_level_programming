627c81357f2adbd8d6b35e9b814317818e966ce9
Ezeikpe <juscraft@yahoo.com>
2022-09-13 18:36:01 -0700

    Going bit by bit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,18 +10,20 @@
 int main(void)
 {
 	long int i, j, k, next;
+
 	j = 1;
 	k = 2;
-	for (i = 1; i <= 50; ++i)
-	{		
-	if (j != 20365011074)	
+for (i = 1; i <= 50; ++i)
+{		
+if (j != 20365011074)	
 	{	
 	printf("%ld, ", j);
+
 	} else
 	{	
-	printf("%ld\n", j);
+printf("%ld\n", j);
 	}	
-	next = j + k;
+next = j + k;
 	j = k;
 	k = next;
 	}
