fd17936c71b37f9ee24b11afecaa8f7ed3d0bbfb
Ezeikpe <juscraft@yahoo.com>
2022-09-15 08:52:24 -0700

    All have commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,12 +7,15 @@
  */
 
 int main(void)
+
 {
+
 int i;
 
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+
 /*
  *while (i < 10)
  *
@@ -25,4 +28,5 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
+
 }
