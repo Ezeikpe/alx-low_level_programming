60ad66d2f1c3042aa52096f07551ba98f992d3b6
Ezeikpe <juscraft@yahoo.com>
2022-09-22 17:13:46 -0700

    Keep it tight

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -32,7 +32,28 @@ void printHexes(char *b, int start, int end)
 		printf("%c", ch);	
 		i++;
 	}
+}
 
+/**
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
 }
 
 /**
