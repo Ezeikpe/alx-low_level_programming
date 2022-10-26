7b07d96397069f8dbcfd3ba15d73631875230f2b
Ezeikpe <juscraft@yahoo.com>
2022-10-25 17:25:15 -0700

    Get it on

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,18 +17,21 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || (*head) == NULL)
 		return (-1);
 
-	if ((*head)->next)
-
-		(*head) = (*head)->next		else
-				(*head) = NULL;
-				free(current);
+	current = *head;
+	if (index == 0)
+	{
+		if ((*head)->next)
+  			(*head) = (*head)->next
+		else
+			(*head) = NULL;
+		free(current);
 			return (1);
-			}
+	}
 
 	while (index != 1)
 	{
 		if (current->next == NULL)
-		return (-1);
+			return (-1);
 
 		current = current->next;
 		--index;
