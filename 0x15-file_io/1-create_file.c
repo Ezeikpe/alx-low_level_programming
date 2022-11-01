5a7d61bfbc87e7643d1f76d70eb628b26b9b8c57
Ezeikpe <juscraft@yahoo.com>
2022-10-31 18:26:07 -0700

    Keep it up

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 	{
-		return (
+		return (-1);
 	}
 	if (text_content)
 	{
