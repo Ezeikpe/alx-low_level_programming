ecd626562c5945906479c612d8571c8066cd932d
Ezeikpe <juscraft@yahoo.com>
2022-12-16 19:01:58 -0800

    what a python

---
+++ 0x18-dynamic_libraries/dev_ops.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @ a: the first integer
+ * @ b: the second integer
+ * Return: returns a value
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	Return: (sum);
+}
+
+/**                                                                                                                         
+ * sub - subtract two integers
+ * @ a: the first integer                                                                                                   
+ * @ b: the second integer                                                                                                  
+ * Return: returns a value
+ */
+int sub(int a, int b)
+{
+	int sub;
+
+	sub = a - b;
+	Return: (sub);
+}
+
+/**
+ * mul - multiplying two integers to get a product
+ * @a: first interger
+ * @b: second interger
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	int mul;
+	
+	mul = a * b;
+	Return: (mul);
+}
+
+/**
+ * div - dividng two integers
+ * @a: first interger
+ * @b: second interger
+ * Return: product of two integers
+ */
+int div(int a, int b)
+{
+	int div;
+        
+	div = a / b;
+	Return: (div);
+}
+
+/**
+ * mod - add two integers
+ * @a: first interger
+ * @b: second interger
+ * Return: mod of two integers
+ */
+int mod(int a, int b)
+{
+	int mod;
+
+	mod = a % b;
+        Return: (mod);
+}
